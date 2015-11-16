9af4825a5c9453dd716043a04c77cab81a49b012
Santosh Kiran Beyagudem <skbeyagu@gmail.com>
2015-11-15 20:44:31 -0500

    button and label

--- 1 Hour Swift tutorial/ViewController.swift
+++ 1 Hour Swift tutorial/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var btnButton: UIButton!
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,8 +22,13 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
     }
 
+    @IBAction func btnButtionAction(sender: AnyObject) {
+        label.text = "Changed"
+    }
 
 }
 
